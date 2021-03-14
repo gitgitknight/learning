884ce3d4edd3a391822c8f1b1e64b2046308e769
snow <ly550956288@126.com>
2021-03-14 23:08:23 +0800

    fix bugs and dev new Function HelloOne

--- src/main/java/HelleOne.java
+++ src/main/java/HelleOne.java
@@ -6,5 +6,11 @@ public class HelleOne {
 
     public static void main(String[] args) {
         System.out.println(123);
+
+        System.out.println("fix bugs");
+    }
+
+    public void HelloOne(){
+        System.out.println("HelloOne");
     }
 }
---
+++ src/main/java/HelloTwo.java
@@ -0,0 +1,6 @@
+/**
+ * @author ly
+ * @create 2021/3/14 - 22:55
+ */
+public class HelloTwo {
+}
